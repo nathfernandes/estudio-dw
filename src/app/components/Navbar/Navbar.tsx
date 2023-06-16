7a91dfa94bd2a5b2ad33234d3e91018d2157407d
Peter <peter@eridanusltda.com>
2023-06-16 15:29:53 -0300

    Fixed navbar, changed colors and created scroll condition

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 66) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+  }, []);
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <img src="../../../../public/ballet.png" alt="Logo" />
-      </div>
+    <div
+      className={`${styles.navbar} ${
+        navbar ? styles.colorNavbar : styles.transparentNavbar
+      }`}
+    >
+      <img src="/images/ballet.png" alt="Logo" className={styles.logo} />
       <div className={styles.links}>
         <a href="#home">Home</a>
       </div>
