b08fcf968bcb13517676c7234d97cc82e7b03551
Peter <peter@eridanusltda.com>
2023-06-16 17:15:56 -0300

    Created About section

---
+++ src/app/components/About/About.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./About.module.css";
+import stylesGlobal from "../../page.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <div className={`${styles.textAbout} ${stylesGlobal.half}`}>
+        <h1 className={styles.titles}>História do Estúdio DW</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipis cinelit, sed do eiusmod
+          tempor incididunt labore et dolore ali. Lorem ipsum dolor sit amet,
+          consectetur adipis cingelit, sed do eiusmod tempor incidi. dunt u
+          labore et dolore magna aliqua. Ut enim ad minim. veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet consectetur adipis
+        </p>
+      </div>
+
+      <div className={styles.aboutImageDiv}>
+        <img src={"/images/about.jpg"} className={styles.aboutImage} />
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <About />
       <Footer />
     </main>
   );
